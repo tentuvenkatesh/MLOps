78f762b8c1c4f9dbd79d2852c9bf25a6f1504099
Venkatesh tentu <tentuvenkatesh2@gmail.com>
2025-09-03 17:25:24 +0530

    Inheritance

---
+++ 2.OOPs/inherit.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(f"{self.name} makes a sound")
+
+class Dog(Animal):
+    # def speak(self):
+    #     print(f"{self.name} barks")
+    pass
+ani = Animal("Fox")
+ani.speak()
+dog = Dog("Buddy")
+dog.speak()
